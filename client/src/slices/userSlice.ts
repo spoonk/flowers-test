aee126dd4a708d3fd1edbb69fe5d4dbc0ad55d9d
spoonk <cornwlucas@gmail.com>
2023-11-21 08:28:31 -0800

    redux quick start (most)

---
+++ client/src/slices/userSlice.ts
@@ -0,0 +1,31 @@
+import { createSlice } from "@reduxjs/toolkit";
+import type { PayloadAction } from "@reduxjs/toolkit";
+
+import { User } from "../types";
+
+export interface UserState {
+  currentUserID: string | undefined;
+  users: User[];
+}
+
+const initialState: UserState = {
+  currentUserID: undefined,
+  users: [],
+};
+
+export const userSlice = createSlice({
+  name: "users",
+  initialState,
+  reducers: {
+    setCurrentUserID: (state, action: PayloadAction<string>) => {
+      state.currentUserID = action.payload;
+    },
+    setUsers: (state, action: PayloadAction<User[]>) => {
+      state.users = action.payload;
+    },
+  },
+});
+
+export const { setCurrentUserID, setUsers } = userSlice.actions;
+
+export default userSlice.reducer;
