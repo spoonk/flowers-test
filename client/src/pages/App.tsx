e30207190ac55d2c46d743dbe27476f7aa5db18b
spoonk <cornwlucas@gmail.com>
2023-11-28 10:44:18 -0800

    slight fe cleanup, make new directories for app pages

--- client/src/pages/App.tsx
+++ client/src/pages/App.tsx
@@ -1,8 +1,7 @@
-import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import AdminPage from "./admin/AdminPage";
+import AdminPage from "../admin/AdminPage";
 
 function App() {
   return (
